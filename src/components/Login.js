6cfcc7996e5d54218e18988151877bb849a0641a
Ashraf-Hamdoun <78766971+Ashraf-Hamdoun@users.noreply.github.com>
2021-05-23 10:46:40 +0200

    add isloggedin condition

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,22 +25,24 @@ class Login extends Component {
   }
 
   render() {
-    const Users = Object.entries(this.props.users).map((User) => {
-      return {
-        user: User[0],
-        infos: User[1],
-      };
-    });
 
-    const showUsers = Users.map((User) => {
+    if (!this.props.isLoggedIn) {
+      const Users = Object.entries(this.props.users).map((User) => {
+        return {
+          user: User[0],
+          infos: User[1],
+        };
+      });
+
+      const showUsers = Users.map((User) => {
+        return (
+          <option key={User.infos.id} value={User.user}>
+            {User.infos.name}
+          </option>
+        );
+      });
+      
       return (
-        <option key={User.infos.id} value={User.user}>
-          {User.infos.name}
-        </option>
-      );
-    });
-
-    return (
       <div className="Log-in-container">
         <h3>Would you rather</h3>
         <h5>select one to login</h5>
@@ -66,12 +68,17 @@ class Login extends Component {
         </Link>
       </div>
     );
+    }
+    return (
+      <h5>you are now logged in</h5>
+    )
   }
 }
 
 function mapStateToProps(state) {
   return {
     users: state.users,
+    isLoggedIn: state.isLoggedIn,
     dir: state.dir,
   };
 }
